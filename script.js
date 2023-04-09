064c558a25da52dfa8b6b83c7c2fd29ef8e6dbaf
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-04-09 12:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-// complete this js code
-function Person(name, age) {}
+// Constructor function for Person
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Add a method to the Person prototype
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Constructor function for Employee
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype in the Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+
+// Set the Employee prototype constructor to Employee
+Employee.prototype.constructor = Employee;
+
+// Add a method to the Employee prototype
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
